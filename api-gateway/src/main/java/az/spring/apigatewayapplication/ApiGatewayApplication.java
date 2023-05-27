b3fc92e95fcc7daecc1d532341ef1bc0a0684f84
Orkhan93 <dokta228@mail.ru>
2023-05-27 09:30:18 +0400

    Added api-gateway implementation

--- api-gateway/src/main/java/az/spring/apigatewayapplication/ApiGatewayApplication.java
+++ api-gateway/src/main/java/az/spring/apigatewayapplication/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package az.spring.productservice;
+package az.spring.apigatewayapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductServiceApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
